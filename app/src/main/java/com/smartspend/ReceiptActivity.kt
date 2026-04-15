467c26cbef4182179b8d09ef0c9bfdf5aa532a13
ST10266958 <st10266958@vcconnect.edu.za>
2026-04-15 13:30:48 +0200

    Designed receipt upload UI screen

---
+++ app/src/main/java/com/smartspend/ReceiptActivity.kt
@@ -0,0 +1,13 @@
+package com.smartspend
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReceiptActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_recipt)
+    }
+}
